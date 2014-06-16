2073fe05dad07fb9adb183e894c1ac2e8f1095a7
fredoprog <fredoprog@gmail.com>
2014-06-16 12:44:25 +0200

    commit "Interface et classe"

--- Code Source/E-Commerce/IProduit.php
+++ Code Source/E-Commerce/IProduit.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
- * Description of Produit
+ * Description of IProduit
  *
- * @author fredericradigoy
+ * @author frederic radigoy
  */
-class Produit {
-    //put your code here
+interface IProduit {
+    
+    public function afficherProdut();
+    public function getNomProduit();
+    public function getPrixProduit();
+    public function getDescription();
+    public function getCheminImage();  
+    
 }
--- Code Source/E-Commerce/Produit.php
+++ Code Source/E-Commerce/Produit.php
@@ -1,33 +1,64 @@
+/*
+
+  @auteur frederic radigoy
+*/
+
+
+
+
+
+
 <?php
 
     class Produit implements IProduit {
-
-        function __construct() {
+        
+        protected $_id;
+        
+        protected $_nomProduit;
+        
+        protected $_cheminImage;
+        
+        protected $_descriptionProduit;
+        
+        protected $_prixProduit;
+                
+               
+        function __construct($id, $nomProduit, $prixProduit, $cheminProduit, $description) {
+            
+            $this->_id = $id;
+            $this->_nomProduit = $nomProduit;
+            $this->_prixProduit = $prixProduit;
+            $this->_cheminImage = $cheminProduit;
+            $this->_descriptionProduit = $description;
 
         }
 
-
+        
         public function afficherProdut() {
-
+            return new Produit();
         }
 
 
         public function getCheminImage() {
-
+            return $this->_cheminImage;
         }
 
 
         public function getDescription() {
+            
+            return $this->_descriptionProduit;
 
         }
 
 
         public function getNomProduit() {
-
+            return $this->_nomProduit;
         }
 
 
         public function getPrixProduit() {
+            
+            return $this->_prixProduit;
 
         }
 
